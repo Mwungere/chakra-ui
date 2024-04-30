cef15b0351115edf70df1f352842953360f74ff1
Mwungere <mwungereelite56@gmail.com>
2024-04-30 07:38:41 +0200

    basics learned

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,7 +7,11 @@ export default function Dashboard() {
     color: "white",
     m: "10px",
     textAlign: "center",
-    filter: "blur(2px)"
+    filter: "blur(2px)",
+    ':hover': {
+      color: "black",
+      bg: "blue.200",
+    }
   }
   return (
     <Container as="section" maxW={"4xl"} py={"20px"}>
