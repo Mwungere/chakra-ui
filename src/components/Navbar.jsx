216f5b6c1ce120fa0a0b05845a6e3e2b63b2e803
Mwungere <mwungereelite56@gmail.com>
2024-04-30 21:07:58 +0200

    profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,31 @@
-import { Box, Flex } from "@chakra-ui/react"
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Heading,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
 
 export const Navbar = () => {
   return (
-    <Flex bg={"gray.200"} justify={"space-between"} wrap={"wrap"} gap={2}>
-        <Box w={"150px"} h={"50px"} bg={"red"}>1</Box>
-        <Box w={"150px"} h={"50px"} bg={"blue"}>2</Box>
-        <Box w={"150px"} h={"50px"} bg={"green"} flexGrow={"1"}>3</Box>
-        <Box w={"150px"} h={"50px"} bg={"yellow"}>4</Box>
+    <Flex as={"nav"} p={"10px"} mb={"40px"} alignItems={"center"}>
+      <Heading as={"h1"}>Dojo Tasks</Heading>
+      <Spacer />
+      <HStack spacing={"20px"}>
+        <Box bg={"gray.200"} p={"10px"}>
+          M
+        </Box>
+        <Text>mario@netninja.dev</Text>
+        <Button colorScheme="purple">Logout</Button>
+      </HStack>
     </Flex>
-  )
-}
+    // <Flex bg={"gray.200"} justify={"space-between"} wrap={"wrap"} gap={2}>
+    //     <Box w={"150px"} h={"50px"} bg={"red"}>1</Box>
+    //     <Box w={"150px"} h={"50px"} bg={"blue"}>2</Box>
+    //     <Box w={"150px"} h={"50px"} bg={"green"} flexGrow={"1"}>3</Box>
+    //     <Box w={"150px"} h={"50px"} bg={"yellow"}>4</Box>
+    // </Flex>
+  );
+};
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,28 @@
-import { Outlet } from "react-router-dom"
-import { Navbar } from "../components/Navbar"
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
+import { Grid, GridItem } from "@chakra-ui/react";
+import { Sidebar } from "../components/Sidebar";
 
 export default function RootLayout() {
   return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
-  )
+    <Grid templateColumns={"repeat(6, 1fr)"} bg={"gray.50"}>
+      <GridItem
+      as={"aside"}
+      colSpan={{base:6, lg:2, xl:1}}
+      bg={"purple.400"}
+      minHeight={{lg:"100vh"}}
+      p={{base:"20px", lg:"30px"}}
+      >
+        <Sidebar />
+      </GridItem>
+      <GridItem
+      as={"main"}
+      colSpan={{base:6, lg:4, xl:5}}
+      p={"40px"}
+      >
+        <Navbar />
+        <Outlet />
+      </GridItem>
+    </Grid>
+  );
 }
