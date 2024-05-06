9880a320d618b3ff17b80138c2dd927ab359b4b5
Mwungere <mwungereelite56@gmail.com>
2024-05-06 11:38:43 +0200

    theme manupilation

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+//extend theme
+
+// const colors= {
+//   brand: {
+//     900: '#024fc9',
+//     800:'#146af5',
+//     700:'#2877f2',
+//     600:'#337df2',
+//     500:'#4287f5'
+//   }
+// }
+
+// const fonts = {
+//   body: 'Tahoma',
+//   heading: 'Courier New'
+// }
+
+// const theme= extendTheme({colors, fonts})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
